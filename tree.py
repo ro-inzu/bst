e2b31f5fe560fee52c7fc5b54cd4df8e223bced8
Rosendo Inzunza <19737771+roinzunza@users.noreply.github.com>
2021-09-13 20:58:57 -0700

    updated tree height fnc

--- tree.py
+++ tree.py
@@ -2,79 +2,73 @@ from node import Node
 
 
 class Tree(object):
-
-    def print_tree(self, node: Node):
+    def in_order(self, node: Node):
         if not node:
             return node
-
-        self.print_tree(node.left)
-        print('{}'.format(node.data), end=' ')
-        self.print_tree(node.right)
-
-    def print_leaves(self,node: Node):
-        if not node:
-            return node
-
-        if not node.left and not node.right:
-            print(node.data, end=' ')
-            return
-
-        if node.left:
-            self.print_leaves(node.left)
-
-        if node.right:
-            self.print_leaves(node.right)
-
-    def tree_depth(self,node: Node):
-        if not node:
-            return 0
-        else:
-            return max(self.tree_depth(node.left), self.tree_depth(node.right)) + 1
-
-    def in_order(self,node: Node):
-        if not node:
-            return node
-
         self.in_order(node.left)
-        print(node.data, end=' ')
+        print(node.data)
         self.in_order(node.right)
 
-    def pre_order(self, node: Node):
-        if not node:
-            return node
+    def tree_height(self, node: Node, height=0):
+        # base case
+        if node is None:
+            return height
+        else:
+            # get height of the most left and right and get the side with most depth
+            return max(self.tree_height(node.left, height + 1), self.tree_height(node.right, height + 1))
 
-        print(node.data, end=' ')
-        self.pre_order(node.left)
-        self.pre_order(node.right)
+    # bfs traversal
+    def level_search(self, root):
+        if not root:
+            return root
 
-    def post_order(self, node: Node):
-        if not node:
-            return node
+        # queue to traverse through the tree
+        queue = [root]
+        while queue:
+            # get current level of tree
+            child = queue.pop(0)
+            print(f"{child.data}")
+            # check if there is a child on left side to traverse through
+            if child.left is not None:
+                queue.append(child.left)
+            # check if there is a child on right side to traverse through
+            if child.right is not None:
+                queue.append(child.right)
 
-        self.post_order(node.left)
-        self.post_order(node.right)
-        print(node.data, end=' ')
+    def depth_search(self, root):
+        if not root:
+            return root
 
-    def depth_first(self, root: Node):
-        if root.left is not None:
-            self.depth_first(root.left)
-        print(root.data, end=' ')
-        if root.right is not None:
-            self.depth_first(root.right)
+        if root.left:
+            self.depth_search(root.left)
 
-    def insert_bst(self, root: Node, data: int):
-        # root passed will check if its empty (passed from left or right) and insert data to node
+        print(f"{root.data}")
 
+        if root.right:
+            self.depth_search(root.right)
+
+    def print_leaves(self, root):
+        # base case
+        if not root:
+            return root
+        # it is a leaf is there is nothing to left and right of the node
+        if root.left is None and root.right is None:
+            print(root.data)
+        # if there is a node on left of current move to left
+        if root.left:
+            self.print_leaves(root.left)
+        if root.right:
+            self.print_leaves(root.right)
+
+    def insert_node(self, root: Node, data: int):
         if root is None:
             root = Node(data)
             return root
-        # binary search tree is not empty, so we will insert it into the tree
-        # if data is less than the root go to the left of the root and call recursively
+
         if data < root.data:
-            root.left = self.insert_bst(root.left, data)
-        # if data is greater than the root go to the right of the root and call recursively
+            root.left = self.insert_node(root.left, data)
         elif data > root.data:
-            root.right = self.insert_bst(root.right, data)
+            root.right = self.insert_node(root.right, data)
         return root
 
     def make_tree(self, elements: list):
